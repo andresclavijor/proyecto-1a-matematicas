6b6058b1fa4992cf336d92f98f5becf999672e02
andresclavijor <shipoca2007@hotmail.com>
2019-12-26 21:25:58 -0500

    primera version de mosulo de matematicas

---
+++ pruebas.js
@@ -0,0 +1,13 @@
+const m = require('.');
+
+console.log(m.suma(1,3));
+console.log(m.suma(12,3));
+
+console.log(m.resta(1,3));
+console.log(m.resta(12,3));
+
+console.log(m.multiplicacion(1,3));
+console.log(m.multiplicacion(12,3));
+
+console.log(m.division(1,3));
+console.log(m.division(12,3));
